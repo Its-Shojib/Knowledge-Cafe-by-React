a28c7bac0f794e9bc1f8012a8fc433e84f280303
Its_Shojib <mdshojib922@gmail.com>
2023-09-12 00:07:52 +0600

    Header Added

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import Header from './component/Header/Header'
+function App() {
+  return (
+    <div className='max-w-screen-xl mx-auto px-4 md:px-8 lg:px-12'>
+      <Header></Header>
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <div className='flex justify-between items-center'>
+            <h1 className='font-bold text-3xl pt-5'>Knowledge Cafe</h1>
+            <img className="pt-5" src="../../../public/profile.png" alt="" />
+        </div>
+    );
+};
+
+export default Header;
