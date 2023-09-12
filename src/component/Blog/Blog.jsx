edac5af4ba528b1ca693f4be12a1fb35fdad7250
Its_Shojib <mdshojib922@gmail.com>
2023-09-12 23:15:54 +0600

    Button Added

--- src/component/Blog/Blog.jsx
+++ src/component/Blog/Blog.jsx
@@ -1,5 +1,6 @@
+import { FaBookmark } from 'react-icons/fa';
 const Blog = ({blog}) => {
-    let {cover,title,author_img,author,posted_date,reading_time} = blog;
+    let {cover,title,author_img,author,posted_date,reading_time,hashtags} = blog;
     return (
         <div className=" space-y-5">
             <img className="w-full" src={cover} alt="" />
@@ -12,11 +13,19 @@ const Blog = ({blog}) => {
                     </div>
                 </div>
                 <div>
-                    <span>{reading_time} min read</span>
-                    <span></span>
+                    <span className='pr-3 text-lg'>{reading_time} min read</span>
+                    <button><FaBookmark></FaBookmark></button>
                 </div>
             </div>
-            <h2 className=" text-3xl font-bold pb-5">{title}</h2>
+            <h2 className=" text-3xl font-bold">{title}</h2>
+            <p>
+                {
+                    hashtags.map((hash, idx) => <span key={idx}><a href="">#{hash}</a> </span>)
+                }
+            </p>
+            <p className='pb-3'>
+                <button>Mark as Read</button>
+            </p>
             
             
         </div>
